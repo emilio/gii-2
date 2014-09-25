cf0f7e5125cfd6d7aa4a563dff7f6399bd09bd69
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-09-25 17:58:13 +0200

    Create test.c

---
+++ tests/bubble-sort/test.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../../AlgorithmTester.h"
+#include "./utils/IntVector.h"
+
+#include "../../AlgorithmTester.c" // Just for quick compilation
+#include "./utils/IntVector.c"
+
+#define BUBBLE_SORT_TEST_RANGE 10000
+
+/**
+ * Bubble sort algorithm
+ * 
+ * @param int * vector
+ * @param size_t length
+ *
+ * @return int * 
+ */
+int * bubbleSort(int * vector, size_t length) {
+	int * sorted = IntVector_clone(vector, length);
+	size_t i, j;
+	int temp; // for swapping
+
+	for ( i = 0; i < length; i++ ) {
+		for ( j = 0; j < length - i - 1; j++ ) {
+			if ( sorted[j] > sorted[j + 1] ) {
+				// SWAP(sorted[j], sorted[j + 1], int);
+				temp = sorted[j];
+				sorted[j] = sorted[j + 1];
+				sorted[j + 1] = temp;
+			}
+		}
+	}
+
+	return sorted;
+}
+
+/**
+ * Test bubble sort
+ *
+ * @param size_t collection_size
+ *
+ */
+void testBubbleSort(size_t length) {
+	int * vector = IntVector__generateInRange(length, 0, BUBBLE_SORT_TEST_RANGE);
+	int * sorted;
+
+	sorted = bubbleSort(vector, length);
+
+	free(vector);
+	free(sorted);
+}
+
+int main() {
+	AlgorithmTester * tester;
+	AlgorithmTesterConfig * config;
+	AlgorithmTesterBenchmark * benchmark;
+
+	srand(time(NULL));
+
+	config = newAlgorithmTesterEmptyConfig();
+	config->min_repetitions = 1000;
+	config->max_execution_time = 2;
+
+	tester = newAlgorithmTester(testBubbleSort, config)
+
+	benchmark = AlgorithmTester_test(tester, 100);
+	AlgorithmTesterBenchmark_toConsole(benchmark);
+
+	free(benchmark);
+	free(config);
+	free(tester);
+
+	return 0;
+}
