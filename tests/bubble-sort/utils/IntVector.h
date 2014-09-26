a0d0a6dff4592cca7f087ac28378eb8ef1de4d1f
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-09-26 10:44:07 +0200

    Create IntVector.h

---
+++ tests/bubble-sort/utils/IntVector.h
@@ -0,0 +1,37 @@
+#ifndef __INT_VECTOR_H
+#define __INT_VECTOR_H
+
+#include <string.h>
+
+typedef int * IntVector;
+
+/**
+ * Allocate a new int vector
+ * @param size_t length
+ *
+ * @return IntVector
+ */
+IntVector newIntVector(size_t length);
+
+/**
+ * Create a vector of `length` elements between `min` and `max`
+ *
+ * @param size_t length
+ * @param int min
+ * @param int max
+ *
+ * @return IntVector
+ */
+IntVector IntVector__generateInRange(size_t length, int min, int max);
+
+/**
+ * Clone an int vector
+ *
+ * @param IntVector vector
+ * @param size_t length
+ *
+ * @return IntVector
+ */
+IntVector IntVector_clone(IntVector vector, size_t length);
+
+#endif
