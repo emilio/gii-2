bdd26339349e552c3b99c8e540a9c0f702bfb691
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-11-24 19:48:50 +0100

    Create test.c

---
+++ AlgorithmTester/tests/radix/test.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../AlgorithmTester.h"
+#include "../../AlgorithmTester.c"
+#define VECTOR_ITEM_TYPE unsigned int
+#include "radix.c"
+
+Vector * generate(size_t length) {
+	size_t i = 0;
+	Vector *v = newVector();
+	for ( ; i < length; i++ ) {
+		Vector_push(v, (VectorItem) rand() % 101 - 50);
+	}
+	return v;
+}
+
+void print(Vector * v) {
+	size_t i = 0;
+	for ( ; i < v->length; i++ ) {
+		printf("%u\t", get(v, i));
+	}
+	printf("\n");
+}
+
+int main() {
+	srand(time(NULL));
+	Vector * unsorted = generate(5);
+
+	print(unsorted);
+
+	radixSort(unsorted);
+
+	print(unsorted);
+
+	return 0;
+}
