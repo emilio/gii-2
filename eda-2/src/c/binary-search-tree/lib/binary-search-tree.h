4f997b52a31aab81174e6d07684dbe30ee3f885a
ecoal95 <ecoal95@gmail.com>
2015-06-10 00:34:04 +0200

    BST: restoring old work due to `rm -rf *`
    Yeah, I'm stupid.

--- eda-2/src/c/binary-search-tree/lib/binary-search-tree.h
+++ eda-2/src/c/binary-search-tree/lib/binary-search-tree.h
@@ -16,125 +16,24 @@ typedef struct bst {
     bst_node_t* head;
 } bst_t;
 
-bst_t* bst_new() {
-    bst_t* ret = (bst_t*) malloc(sizeof(bst_t));
+bst_t* bst_new();
 
-    assert(ret);
+bst_node_t* bst_node_new(bst_value_t val);
 
-    ret->head = NULL;
-    return ret;
-}
+void bst_insert(bst_t* self, bst_value_t val);
 
-bst_node_t* bst_node_new(bst_value_t val) {
-    bst_node_t* ret = (bst_node_t*) malloc(sizeof(bst_node_t));
+void bst_debug(bst_t* self);
 
-    assert(ret);
+void bst_node_debug(bst_node_t* self, int depth);
 
-    ret->left = ret->right = NULL;
-    ret->balance_factor = 0;
-    ret->value = val;
-
-    return ret;
-}
-
-void bst_insert(bst_t* self, bst_value_t val) {
-    unsigned char changed = 0;
-    bst_node_t* node = bst_node_new(val);
-    if ( ! self->head )
-        self->head = node;
-    else
-        bst_node_insert(self->head, &changed, node);
-}
-
-// Rotate a node to the right
-// TODO: Review carefully
-void bst_node_rotate_right(bst_ndoe_t* self) {
-    bst_value_t temp_value;
-
-    assert(self->left);
-
-    // We swap by value, and move childrens to the right
-    temp_value = self->value;
-    self->value = self->left->value;
-    self->left->value = temp_value;
-
-    self->right = self->left;
-    self->left = self->left->left;
-    self->right->left = self->left->right;
-}
+void bst_node_rotate_right(bst_node_t* self);
 
 // The same thing, but to the left
-void bst_node_rotate_left(bst_node_t*self) {
-    bst_value_t temp_value;
-
-    temp_value = self->value;
-    self->value = self->right->value;
-    self->right->value = temp_value;
-
-    self->left = self->right;
-    self->right = self->right->right;
-    self->left->right = self->right->left;
-}
+void bst_node_rotate_left(bst_node_t*self);
 
 // We lost balance, reestructure our children
-void bst_node_reestructure(bst_node_t* self) {
-    bst_node_t* temp_node;
-    bst_value_t temp;
-    if ( self->balance_factor == -2 ) { // The left child has increased
-        if ( self->left->balance_factor == -1 ) { // left-left -> rotate_right()
-            bst_node_rotate_right(self);
-        }
+void bst_node_reestructure(bst_node_t* self);
 
-    }
-
-}
-
-void bst_node_insert(bst_node_t* self, unsigned char* height_changed, bst_node_t* node) {
-    bst_node_t* parent = self;
-    // We must insert it in the left
-    // of the tree
-    if ( self->value < val ) {
-        if ( ! self->left ) {
-            self->left = node;
-            self->balance_factor--;
-
-            // If the height has_changed
-            if ( ! self->right )
-                *height_changed = 1;
-        } else {
-            bst_node_insert(self->left, height_changed, node);
-            // The height of our left child has changed, so our balance factor
-            // and posibly our height too
-            if ( *height_changed ) {
-                self->balance_factor--;
-                if ( self->balance_factor == 0) // Our height has not changed
-                    *height_changed = 0;
-                else if ( self->balance_factor == -2 ) // Our height has changed, and we have lost balance
-                    bst_node_reestructure(self);
-                // else our height changed, but we haven't lost balance
-                // so no need to touch
-            }
-
-        }
-    // Else... on the right
-    } else {
-        if ( ! self->right ) {
-            self->right = node;
-            self->balance_factor++;
-
-            if ( ! self->left )
-                *height_changed = 1;
-        } else {
-            bst_node_insert(self->right, height_changed, node);
-            if ( *height_changed ) {
-                self->balance_factor++;
-                if ( self->balance_factor == 0 )
-                    *height_changed = 0;
-                else if ( self->balance_factor == 2 )
-                    bst_node_reestructure(self);
-            }
-        }
-    }
-}
+void bst_node_insert(bst_node_t* self, unsigned char* height_changed, bst_node_t* node);
 
 #endif
